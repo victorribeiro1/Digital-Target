030d72283b10ef807fe755457b1fe83e260916a0
victorribeiro1 <victorribeiro2929@gmail.com>
2022-09-17 17:31:42 -0300

    Inclusão do PHP

--- formulario.php
+++ formulario.php
@@ -9,9 +9,19 @@
         <link rel="stylesheet" href="assets/scss/formulario.css">
     </head>
     <body>
+
+    <?php
+        if (($_SERVER['REQUEST_METHOD'] !== 'POST')) {
+            echo '
+                // <script>
+                //     window.location.href = "http://consultoria.digitaltarget.com.br";
+                // </script>    
+            ';
+        }
+    ?>
+
         <div class="formulario">
             <header class="formulario__header">
-
                 <div class="formulario__header__texto">
                     <p class="formulario__header__texto__paragrafo">
                         Solicitamos que leia atentamente a cada uma das perguntas e responda de maneira consciente e verdadeira. 
@@ -27,7 +37,14 @@
                 </div>
             </header>
 
-            <form id="formulario" action="obrigado.php" class="formulario__forms">
+            <form id="formulario" action="obrigado.php" method="post" class="formulario__forms">
+                <?php
+                echo '
+                    <input type="hidden" name="nome" value='.$_POST["nome"].'>    
+                    <input type="hidden" name="email" value='.$_POST["email"].'>    
+                    <input type="hidden" name="numero" value='.$_POST["numero"].'>
+                ';
+                ?>
                 <div class="formulario__forms__pergunta">
                     <h2 class="formulario__forms__pergunta__titulo">01) De 1 a 10, onde 1 significa muito pouco e 10 significa muito. Qual o nível de comprometimento que você tem em melhorar os resultados do seu negócio?</h2>
 
@@ -45,80 +62,81 @@
                             <span class="formulario__forms__pergunta__container-range__numeros__numero">10</span>
                         </div>
 
-                        <input type="range" name="pergunta2" class="formulario__forms__pergunta__container-range__input" min="1" max="10" step="1">
+                        <input type="range" name="pergunta1" class="formulario__forms__pergunta__container-range__input" min="1" max="10" step="1">
                     </div>
                 </div>
 
                 <div class="formulario__forms__pergunta">
                     <h2 class="formulario__forms__pergunta__titulo">02) Que tipo de negócio é o seu? (negócio local, e-commerce, vende produtos ou serviços)</h2>
-                    <input type="text" name="pergunta1" placeholder="Insira sua resposta aqui" class="formulario__forms__pergunta__input">
+                    <input type="text" name="pergunta2" placeholder="Insira sua resposta aqui" class="formulario__forms__pergunta__input">
                 </div>
                 
                 <div class="formulario__forms__pergunta">
                     <h2 class="formulario__forms__pergunta__titulo">03) Quanto fatura mensalmente?</h2>
-                    <input type="text" name="pergunta1" placeholder="Insira sua resposta aqui" class="formulario__forms__pergunta__input">
+                    <input type="text" name="pergunta3" placeholder="Insira sua resposta aqui" class="formulario__forms__pergunta__input">
                 </div>
                 
                 <div class="formulario__forms__pergunta">
                     <h2 class="formulario__forms__pergunta__titulo">04) Qual ou quais objetivo(s) definiu para os próximos 12 meses?</h2>
-                    <input type="text" name="pergunta1" placeholder="Insira sua resposta aqui" class="formulario__forms__pergunta__input">
+                    <input type="text" name="pergunta4" placeholder="Insira sua resposta aqui" class="formulario__forms__pergunta__input">
                 </div>
 
                 <div class="formulario__forms__pergunta">
                     <h2 class="formulario__forms__pergunta__titulo">05) Quais os principais desafios que você tem nesse momento:</h2>
                     <div class="formulario__forms__pergunta__container-checkbox">
                         <div class="formulario__forms__pergunta__container-checkbox__opcao">
-                            <input type="checkbox"  name="pergunta3a" id="pergunta3a" placeholder="pergunta3a" class="formulario__forms__pergunta__container-checkbox__opcao__checkbox"> <label for="pergunta3a" class="formulario__forms__pergunta__container-checkbox__opcao__label">pergunta 3 a</label>
+                            <input type="checkbox"  name="pergunta5a" id="pergunta3a" placeholder="pergunta3a" class="formulario__forms__pergunta__container-checkbox__opcao__checkbox"> <label for="pergunta3a" class="formulario__forms__pergunta__container-checkbox__opcao__label">pergunta 3 a</label>
                         </div>
                         
                         <div class="formulario__forms__pergunta__container-checkbox__opcao">
-                            <input type="checkbox"  name="pergunta3b" id="pergunta3b" placeholder="pergunta3b" class="formulario__forms__pergunta__container-checkbox__opcao__checkbox"> <label for="pergunta3b" class="formulario__forms__pergunta__container-checkbox__opcao__label">pergunta 3 b</label>
+                            <input type="checkbox"  name="pergunta5b" id="pergunta3b" placeholder="pergunta3b" class="formulario__forms__pergunta__container-checkbox__opcao__checkbox"> <label for="pergunta3b" class="formulario__forms__pergunta__container-checkbox__opcao__label">pergunta 3 b</label>
                         </div>
                         
                         <div class="formulario__forms__pergunta__container-checkbox__opcao">
-                            <input type="checkbox"  name="pergunta3c" id="pergunta3c" placeholder="pergunta3c" class="formulario__forms__pergunta__container-checkbox__opcao__checkbox"> <label for="pergunta3c" class="formulario__forms__pergunta__container-checkbox__opcao__label">pergunta 3 c</label>
+                            <input type="checkbox"  name="pergunta5c" id="pergunta3c" placeholder="pergunta3c" class="formulario__forms__pergunta__container-checkbox__opcao__checkbox"> <label for="pergunta3c" class="formulario__forms__pergunta__container-checkbox__opcao__label">pergunta 3 c</label>
                         </div>
                     </div>
-                    <input type="text"      name="pergunta3c" placeholder="Outro" class="formulario__forms__pergunta__input">
+                    <input type="text"      name="pergunta5d" placeholder="Outro" class="formulario__forms__pergunta__input">
                 </div>
 
                 <div class="formulario__forms__pergunta">
                     <h2 class="formulario__forms__pergunta__titulo">06) É importante resolver o desafio apontando na pergunta anterior para a sua empresa?</h2>
-                    <input type="text" name="pergunta1" placeholder="Insira sua resposta aqui" class="formulario__forms__pergunta__input">
+                    <input type="text" name="pergunta6" placeholder="Insira sua resposta aqui" class="formulario__forms__pergunta__input">
                 </div>
                 
                 <div class="formulario__forms__pergunta">
                     <h2 class="formulario__forms__pergunta__titulo">07) Quais os benefícios você vê para o seu negócio no uso das estratégias digitais e anúncios patrocinados?</h2>
-                    <input type="text" name="pergunta1" placeholder="Insira sua resposta aqui" class="formulario__forms__pergunta__input">
+                    <input type="text" name="pergunta7" placeholder="Insira sua resposta aqui" class="formulario__forms__pergunta__input">
                 </div>
                 
                 <div class="formulario__forms__pergunta">
                     <h2 class="formulario__forms__pergunta__titulo">08) Ajudaria se você pudesse ter clientes, não só de uma compra, mas clientes de compras
                         recorrentes?</h2>
-                    <input type="text" name="pergunta1" placeholder="Insira sua resposta aqui" class="formulario__forms__pergunta__input">
+                    <input type="text" name="pergunta8" placeholder="Insira sua resposta aqui" class="formulario__forms__pergunta__input">
                 </div>
                 
                 <div class="formulario__forms__pergunta">
                     <h2 class="formulario__forms__pergunta__titulo">09) Qual o grau de importância em ter relatórios quinzenais ou mensais dos resultados obtidos com as estratégias e anúncios digitais, para auxiliar na tomada de decisão do seu negócio?</h2>
-                    <input type="text" name="pergunta1" placeholder="Insira sua resposta aqui" class="formulario__forms__pergunta__input">
+                    <input type="text" name="pergunta9" placeholder="Insira sua resposta aqui" class="formulario__forms__pergunta__input">
                 </div>
                 
                 <div class="formulario__forms__pergunta">
                     <h2 class="formulario__forms__pergunta__titulo">10) Estar mais bem avaliado pelos clientes, quando comparado aos concorrentes, é importante para o seu negócio?</h2>
-                    <input type="text" name="pergunta1" placeholder="Insira sua resposta aqui" class="formulario__forms__pergunta__input">
+                    <input type="text" name="pergunta10" placeholder="Insira sua resposta aqui" class="formulario__forms__pergunta__input">
                 </div>
                 
                 <div class="formulario__forms__pergunta">
                     <h2 class="formulario__forms__pergunta__titulo">11) Faz sentido para você ter uma empresa e profissionais que trabalham em parceria e com proximidade ao seu negócio?</h2>
-                    <input type="text" name="pergunta1" placeholder="Insira sua resposta aqui" class="formulario__forms__pergunta__input">
+                    <input type="text" name="pergunta11" placeholder="Insira sua resposta aqui" class="formulario__forms__pergunta__input">
                 </div>
                 
                 <div class="formulario__forms__pergunta">
                     <h2 class="formulario__forms__pergunta__titulo">12) Qual a verba mensal destinada para anúncios?</h2>
-                    <input type="text" name="pergunta1" placeholder="Insira sua resposta aqui" class="formulario__forms__pergunta__input">
+                    <input type="text" name="pergunta12" placeholder="Insira sua resposta aqui" class="formulario__forms__pergunta__input">
                 </div>
 
-                <button type="submit" class="formulario__forms__botao">Enviar</button>
+                <!-- <button type="submit" name="enviar" class="formulario__forms__botao">Enviar</button> -->
+                <input type="submit" name="enviar" value="Enviar" class="formulario__forms__botao">
             </form>
         </div>
     </body>
---
+++ obrigado.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+    <html lang="pt-BR">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Obrigado!</title>
+
+        <link rel="stylesheet" href="assets/scss/obrigado.css">
+
+        <style>
+            * {
+                color: var(--cor-fundo);
+            }
+        </style>
+    </head>
+    <body>
+
+    <?php
+        //Import PHPMailer classes into the global namespace
+        //These must be at the top of your script, not inside a function
+        use PHPMailer\PHPMailer\PHPMailer;
+        use PHPMailer\PHPMailer\SMTP;
+        use PHPMailer\PHPMailer\Exception;
+
+        //Load Composer's autoloader
+        require 'vendor/autoload.php';
+
+        if (($_SERVER['REQUEST_METHOD'] === 'POST')) {
+            //Create an instance; passing `true` enables exceptions
+            $mail = new PHPMailer(true);
+
+        try {
+            //Server settings
+                $mail->isSMTP();                                            //Send using SMTP
+                $mail->CharSet = 'UTF-8';
+                $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+                $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+                $mail->Username   = 'victorribeiro.dev@gmail.com';                     //SMTP username
+                $mail->Password   = 'zhyqwtlcyofnotqk';                               //SMTP password
+                $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
+                $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+                //Recipients
+                $mail->setFrom('victorribeiro.dev@gmail.com', $_POST['nome']);
+                $mail->addAddress('victorribeiro.dev@gmail.com', 'Consultoria');     //Add a recipient
+                $mail->addReplyTo('victorribeiro.dev@gmail.com', 'Information');
+
+                //Content
+                $mail->isHTML(true);                                  //Set email format to HTML
+                $mail->Subject = 'Formulário de Qualificação';
+                $body = "
+                Nome: ". $_POST['nome']. "<br />
+                E-mail: ". $_POST['email']. "<br />
+                Número: ". $_POST['numero']. "<br /><br />
+                01): ". $_POST['pergunta1']. "<br />
+                02): ". $_POST['pergunta2']. "<br />";
+
+                $mail->Body = $body;
+
+                $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+                $mail->send();
+
+                echo '
+
+                <div class="obrigado">
+                    <div class="obrigado__container">
+                    <div class="obrigado__container__container-icone">
+                            <div class="obrigado__container__container-icone__circulo"></div>
+                            <svg class="icon obrigado__container__container-icone__icone">
+                                <use xlink:href="assets/midias/icones/symbol-defs.svg#checkmark"></use>
+                            </svg>
+                        </div>
+    
+                        <p class="obrigado__container__texto">
+                            <span class="obrigado__container__texto__linha">
+                                Agradecemos por se cadastrar e preencher o formulário de qualificação.
+                            </span>
+        
+                            <span class="obrigado__container__texto__linha">
+                                <span class="obrigado__container__texto__linha__destaque">importante</span>: Estaremos analisando as informações e daremos o retorno por e-mail com a
+                            </span>
+    
+                            <span class="obrigado__container__texto__linha">
+                                aprovação ou não do envio da proposta de serviços para o seu negócio. 
+                            </span>
+                        </p>
+        
+                        <div class="obrigado__container__redes-sociais">
+                            <p class="obrigado__container__redes-sociais__texto">Nossas redes sociais:</p>
+                            <div class="obrigado__container__redes-sociais__container">
+                                <a href="" class="obrigado__container__redes-sociais__container__link">
+                                    <svg class="icon obrigado__container__redes-sociais__container__link__icone obrigado__container__redes-sociais__container__link__icone--1">
+                                        <use xlink:href="assets/midias/icones/symbol-defs.svg#instagram"></use>
+                                    </svg>
+                            </a>
+                                <a href="" class="obrigado__container__redes-sociais__container__link">
+                                    <svg class="icon obrigado__container__redes-sociais__container__link__icone obrigado__container__redes-sociais__container__link__icone--2">
+                                        <use xlink:href="assets/midias/icones/symbol-defs.svg#linkedin"></use>
+                                    </svg>
+                                </a>
+                                <a href="" class="obrigado__container__redes-sociais__container__link">
+                                    <svg class="icon obrigado__container__redes-sociais__container__link__icone obrigado__container__redes-sociais__container__link__icone--3">
+                                        <use xlink:href="assets/midias/icones/symbol-defs.svg#facebook"></use>
+                                    </svg>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>';
+
+                } catch (Exception $e) {
+                    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+                }
+
+            } else {
+                echo '
+                    <script>
+                        window.location.href = "http://consultoria.digitaltarget.com.br";
+                    </script>    
+                ';
+            }
+        ?>
+    </body>
+</html>
